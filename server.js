b43019f7e723b158fe2d809856455814da7b9628
saipavank <saipavankoppu@gmail.com>
2017-08-02 21:49:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req,res){
+app.get('/first-page', function (req,res){
     res.send('first page is opened');
     
 });
