ca7840368334c5bb72296cddf82edfcedc921ae3
saipavank <saipavankoppu@gmail.com>
2017-08-03 13:59:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,13 @@
-console.log('Loaded!');
-var img = document.getElementById('koushik');
-
-    img.onclick= function()
-    {
-        img.style.marginLeft='100px';
-    };
+ var  button = document.getElementById("counter")
+ var counter =0;
+ button.onclick= function()
+ {
+     
+     
+     
+     counter= counter + 1;
+     var span = document.getElementById("count")
+     span.innerHTML= counter.toString()
+     
+     
+ };
