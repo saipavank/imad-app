45bdc68051f4606d5e17774fab01d14e4997939d
saipavank <saipavankoppu@gmail.com>
2017-08-03 14:21:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,25 @@
  var  button = document.getElementById('counter');
- var counter =0;
  button.onclick = function()
  {
+     var request = new XMLHttpRequest();
      
+     request.onreadystatechange = function()
+     {
+         if(request.readystate === XMLHttp.DONE)
+         {
+             if(request.status === 200){
+                 var counter = request.Responsetext;
+                  var span = document.getElementById('count');
+                  span.innerHTML= counter.toString();
+                 
+             }
+         }
+         
+     };
+    request.open('GET','http://saipavankoppu.imad.hasura.io/counter' );
+    request.send(null);
      
-     
-     counter= counter + 1;
-     var span = document.getElementById('count');
-     span.innerHTML= counter.toString();
+    
      
      
  };
