e36a9fb0c0f86065f6c3736c7d804cda5dd952ef
saipavank <saipavankoppu@gmail.com>
2017-08-07 23:40:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool = require('pg').pool;
+var config{
+     user: 'saipavankoppu',
+    database:'saipavankoppu',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD;
+    };
 var app = express();
 app.use(morgan('combined'));
 
@@ -9,6 +16,23 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
 
+var pool = new pool(config);
+app.get('/test-db', function(){
+    
+    pool.query('SELECT * FROM test ', function(err,res){
+       
+       if(err){
+           res.status(505).send(err.toString());
+           
+       }
+        else
+        {
+            res.send('JSON.Stringfy'(result));
+        }
+    )};
+    
+    
+});
 var counter=0;
 app.get('/counter',function(req,res)
 {
